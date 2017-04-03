c0b083fc0a5a9f176ee4bd22ca5c9118b5e66f24
NORDBY\Mathias <Mathias_nordby@hotmail.com>
2017-04-03 10:48:31 +0200

    php

--- php/insert.php
+++ php/insert.php
@@ -6,20 +6,8 @@
  * Time: 10.20
  */
 
-$address = 'localhost';
-$user = 'root';
-$pass = 'root';
-$dbName = 'Insert name here';
-
-$con = mysqli_connect($address,$user,$pass);
-
-if(!$con){
-    echo 'Not connected to server ';
-}
-
-if(!mysqli_select_db($con,$dbName)){
-    echo 'Database not selected';
-}
+include("dbConnection.php");
+session_start();
 
 $title = $_POST[''];
 $imageURL = $_POST[''];
