a8c59bf0a6dc361c11b6a621f6152cff78cccff5
ThorNissen <thorjnissen@gmail.com>
2017-04-03 10:28:25 +0200

    insert.php Create added
    CRUD
    Create added
    TBD: RUD

--- insert.php
+++ insert.php
@@ -6,3 +6,29 @@
  * Time: 10.20
  */
 
+$address = 'localhost';
+$user = 'root';
+$pass = 'root';
+$dbName = 'Insert name here';
+
+$con = mysqli_connect($address,$user,$pass);
+
+if(!$con){
+    echo 'Not connected to server ';
+}
+
+if(!mysqli_select_db($con,$dbName)){
+    echo 'Database not selected';
+}
+
+$titel = $_POST[''];
+$imageURL = $_POST[''];
+$paragraph = $_POST[''];
+
+$sql = "INSERT INTO nameOfDB (titel,imageURL,paragraph) VALUES ('$titel','$imageURL','$paragraph')";
+
+if(!mysqli_query($con,$sql)){
+    echo 'Not inserted';
+}
+
+header('Location: index.html');
