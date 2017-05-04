7b258f20100e37df6d32d0d847994ef1170f4465
Surface Pro 4\Nicki <nicki_1992@hotmail.com>
2017-05-04 10:09:38 +0200

    Spørgsmål

--- JS/my.js
+++ JS/my.js
@@ -2,11 +2,7 @@
  * Created by Nicki on 04-05-2017.
  */
 
-function togglePrimary(id) {
-    $.POST("php/primary.php", function (data){
-        call primary().php with id;
-    });
-
-    POST php/primary.php
+function togglePrimary(id) { //id bliver sendt videre fra knappen på artiklen
+    // id skal sendes til primary.php
 
 }
