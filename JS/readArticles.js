1e1cf9d3dc801009e7b0a6da6610afa134bfe478
Surface Pro 4\Nicki <nicki_1992@hotmail.com>
2017-05-04 11:03:20 +0200

    move to primary almost done

--- JS/readArticles.js
+++ JS/readArticles.js
@@ -18,7 +18,6 @@ function deleteArticles(id) {
 function getArticles() {
     $.get("php/read.php", function (data){
         result = JSON.parse(data);
-        console.log
         $("#articles").clear;
         for(i = 0; i < result.length; i++){
             article = result[i];
@@ -39,4 +38,31 @@ function getArticles() {
             )
         }
     });
+
+    // Update primary articles
+    $.get("php/readPrimary.php", function (data){
+        result = JSON.parse(data);
+        $("#articles").clear;
+        for(i = 0; i < result.length; i++){
+            article = result[i];
+            $("#primaryArticle").append(
+                '<tr>' +
+                '<td>' + article.id + '</td>' +
+                '<td>' + article.title + '</td>' +
+                '<td>' + article.image + '</td>' +
+                '<td>' + article.content + '</td>' +
+
+                '<td>' +
+                '   <a href="updateArticlePage.php?id=' + article.id+ '&title='+ article.title+'&content='+article.content+'&image='+article.image+'">' +
+                '       <button class="btn btn-success">Update</button>' +
+                '</a></td>' +
+                '<td><button class="btn btn-danger" onclick="deleteArticles(' + article.id + ')">Delete</button></td>' +
+                '</tr>'
+            )
+        }
+    });
+
+
+
+
 }
