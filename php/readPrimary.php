add619bc1c10ed7f7116e4f4959d5b8b0b7f45c8
Surface Pro 4\Nicki <nicki_1992@hotmail.com>
2017-05-04 10:54:04 +0200

    Move to primary

--- php/readPrimary.php
+++ php/readPrimary.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Nexoe
- * Date: 03-04-2017
- * Time: 12:23
+ * Created by IntelliJ IDEA.
+ * User: Nicki
+ * Date: 04-05-2017
+ * Time: 10:52
  */
 
 include ("dbConnection.php");
 session_start();
 
-$sql = "SELECT * FROM articles WHERE isDeleted = 0 AND isPrimary = 0";
+$sql = "SELECT * FROM articles WHERE isDeleted = 0 AND isPrimary = 1";
 
 $result = $conn->query($sql);
 $rows = array();
